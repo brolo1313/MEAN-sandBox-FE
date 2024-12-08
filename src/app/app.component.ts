eaf3de04b06a6374110beda167fbfaa43f774be3
Andrew Yupin <brolo1341@gmail.com>
2024-12-08 15:24:39 +0200

    CI: added initial github workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +10,5 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
 
-  constructor(private http: HttpClient) {
-  }
-
-  ngOnInit() {
-  }
-  
 }
